05ab7d21393dcbdcc1d83ae42f4d8da706060d4a
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-12-27 19:30:05 -0700

    Add minimal module setup

---
+++ scripts/_amd.ts
@@ -0,0 +1,40 @@
+/**
+ * AMD implementation for local modules defined in a single file only.
+ * Needs to be the first thing included by TypeScript in the outFile.
+ * Circular references between the top level of two modules will either produce
+ * a type error or will silently fail.  Don't do that.
+ */
+
+interface Window {
+  require: (id: string) => any;
+  define: (id: string, dependencies: string[], factory: Function) => any;
+}
+
+(function(){
+  const registry: { [id: string]: {
+    dependencyIds: string[];
+    factory: Function;
+  } } = {};
+
+  const instances: { [id: string]: object } = {};
+
+  window.require = instances.require = function require(id: string): object {
+    instances[id] = {};
+
+    const dependencies = registry[id].dependencyIds.map(function(dependencyId) {
+      if (dependencyId === 'exports') {
+        return instances[id];
+      }
+
+      return instances[dependencyId] || require(dependencyId);
+    });
+
+    registry[id].factory.apply(null, dependencies);
+
+    return instances[id];
+  }
+
+  window.define = function(id: string, dependencyIds: string[], factory: Function): void {
+    registry[id] = { dependencyIds, factory };
+  };
+})();
